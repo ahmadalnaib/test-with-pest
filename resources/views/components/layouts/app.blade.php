5876fe34e695ce825e0a4a27711f540dfb757819
ahmedhabeeb73 <alnaib888@gmail.com>
2025-02-11 18:58:35 +0100

    create layout

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @else
+     
+        @endif
+    </head>
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
+        <div class="max-w-4xl mx-auto px-6 grid grid-cols-8  gap-12  mt-12">
+            <div class="col-span-2 border-r border-gray-200 space-y-6">
+            <ul class="">
+                <li>
+                    <span class="font-bold text-lg text-slate-400 hover:text-slate-600 block py-1" href="">Ahmad</span>
+                </li>
+                <li>
+                    <a class="font-bold text-lg text-slate-400 hover:text-slate-600 block py-1" href="">Feed</a>
+                </li>
+            </ul>
+
+            <ul class="">
+               
+                <li>
+                    <a class="font-bold text-lg text-slate-400 hover:text-slate-600 block py-1" href="">my book</a>
+                </li>
+            </ul>
+     
+          
+            </div>
+<div class="col-span-6">
+    @isset($header)
+    <h1 class="font-bold text-2xl text-slate-800">
+        {{ $header }}
+    </h1>
+        
+    @endisset
+    
+    {{ $slot }}
+</div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app>
+    <x-slot name="header">
+ Home
+    </x-slot>
+
+    <div class="mt-8">
+    Hi
+    </div>
+</x-layouts.app>
